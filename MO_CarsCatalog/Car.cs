4e9dcbf0c1c69c6bad155cb119f2116f990c6c3c
mostapciuc <sweetotch@mail.ru>
2024-03-06 16:41:12 +0000

    [mo] - implement car entity.

---
+++ MO_CarsCatalog/Car.cs
@@ -0,0 +1,75 @@
+﻿namespace MO_CarsCatalog
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Car
+    {
+        private int     wheelDrive;
+        private int     topSpeed;
+        private string  mark;
+        private string  model;
+
+        public Car ()
+        {
+            this.wheelDrive = 0;
+            this.topSpeed   = 0;
+            this.mark       = "";
+            this.model      = "";
+        }
+
+        public Car (int wheelDrive = 0, int topSpeed = 0, string mark = "", string model = "")
+        {
+            this.wheelDrive = wheelDrive;
+            this.topSpeed   = topSpeed;
+            this.mark       = mark;
+            this.model      = model;
+        }
+
+        public void setWheelDrive (int wheelDrive = 0)
+        {
+            this.wheelDrive = wheelDrive;
+        }
+        public void setTopSpeed (int topSpeed = 0)
+        {
+            this.topSpeed = topSpeed;
+        }
+        public void setMark (string mark = "")
+        {
+            this.mark = mark;
+        }
+        public void setModel (string model = "")
+        {
+            this.model = model;
+        }
+
+        public int getWheelDrive()
+        {
+            return this.wheelDrive;
+        }
+        public int getTopSpeed()
+        {
+            return this.topSpeed;
+        }
+        public string getMark()
+        {
+            return this.mark;
+        }
+        public string getModel()
+        {
+            return this.model;
+        }
+
+        public void displayCarInfo()
+        {
+            Console.WriteLine($"\n==== {this.mark} ====");
+            Console.WriteLine($"Model: {this.model}");
+            Console.WriteLine($"Drive wheels: {this.wheelDrive}");
+            Console.WriteLine($"Top speed: {this.topSpeed}");
+        }
+
+    }
+}
--- MO_CarsCatalog/Program.cs
+++ MO_CarsCatalog/Program.cs
@@ -10,6 +10,21 @@
     {
         static void Main(string[] args)
         {
+            Car bmw_e34 = new Car();
+
+            bmw_e34.setWheelDrive(2);
+            bmw_e34.setTopSpeed(230);
+            bmw_e34.setMark("BMW");
+            bmw_e34.setModel("e34");
+
+
+            Car mercedes_w140 = new Car(2, 200, "Mercedes-Benz", "w140");
+
+            bmw_e34.displayCarInfo();
+            mercedes_w140.displayCarInfo();
+
+            Console.ReadKey();
         }
+
     }
 }
