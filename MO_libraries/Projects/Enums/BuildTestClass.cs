51539b139cad2e591442e46f3edcf4943e5b808f
mostapciuc <sweetotch@mail.ru>
2024-03-29 10:33:25 +0000

    [mostapciuc] - Re-organize the cars project in repository

---
+++ MO_libraries/Projects/Enums/BuildTestClass.cs
@@ -0,0 +1,16 @@
+﻿namespace EnumsCar
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class BuildTestClass
+    {
+         public static void main(string[] _args)
+        {
+            Console.WriteLine("11123132");
+        }
+    }
+}
